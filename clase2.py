7c81447b29915b68d9cbfef6a79b79d360ababdf
JoaquínC <81444960+JoaquinCabral@users.noreply.github.com>
2021-04-28 09:41:46 -0300

    Add files via upload

---
+++ clase2.py
@@ -0,0 +1,127 @@
+#Ejercicio 1
+# numero = int(input("ingrese un numero"))
+# if(numero < 19):
+#     print("el numero es menor que 19")
+
+# print("fin del algoritmo")
+
+# numero = int(input("ingrese un numero"))
+# if(numero < 19):
+#     print("el numero es menor que 19")
+# else:
+#     print("el numero es mayor que 19")
+
+# print("fin del algoritmo")
+
+#Ejercicio 2
+# numero = int(input("ingrese un numero"))
+
+# if(numero > 0):
+#     print("el numero es positivo")
+
+# else:
+#     if(numero < 0):
+#         print("el numero es negativo")
+#     else:
+#         print("el numero es neutro")
+
+# print("fin del algoritmo")
+
+#Ejercicio 3
+# numero = int(input("ingrese un numero"))
+
+# if(numero % 2 == 0):
+#     print("el numero es par")
+
+# else:
+#     print("el numero es impar")
+
+# print("fin del algoritmo")
+
+#Ejercicio 4
+# nota_final = float(input("ingrese la suma de todas las notas que se saco x alumno"))
+
+# if(nota_final >= 6):
+#     print("el alumno esta aprobado")
+
+# else:
+#     print("el alumno esta desaprobado")
+
+# print("fin del algoritmo")
+
+#Ejercicio 5
+# año_nacimiento = int(input("ingrese el año de nacimiento"))
+# año_actual = int(input("ingrese el año actual"))
+# edad_minima = int(input("ingrese la edad minima del puesto"))
+
+# if(edad_minima >= 18):
+#     print("la edad minima es 18")
+
+# edad_postulantes = año_nacimiento - año_actual
+
+# if(edad_postulantes):
+#     print("el postulante tendra la edad necesaria para llenar la vacante")
+
+# else:
+#     print("el postulante NO tendra la edad necesaria para llenar la vacante")
+
+# print("fin del algoritmo")
+
+#Ejrcicio 6
+# numero1 = int(input("ingrese un numero"))
+# numero2 = int(input("ingrese un numero"))
+
+# if(numero1 == numero2):
+#     print("son iguales")
+
+# elif(numero1 > numero2):
+#     print("el numero1 es el mayor")
+# else:
+#     print("el numero2 es el mayor")
+
+# print("fin del algoritmo")
+
+#Ejercicio 7
+# tarjeta = input("ingrese el nombre de la tarjeta")
+
+# if(tarjeta == "visa"):
+#     print("tarjeta visa")
+
+# else:
+#     print("otra tarjeta")
+
+# print("fin del algoritmo")
+
+#Ejercicio 8
+# monto = float(input("ingrese el monto "))
+# tarjeta = input("ingrese tarjeta")
+# # cuotas = int(input("ingrese el numero de cuotas"))
+
+# # if(cuotas == 3):
+# #     monto = monto * 1.03
+# # elif(cuotas == 8):
+# #     monto = monto * 1.17
+# # elif(cuotas == 12):
+# #     monto = monto * 1.32
+
+# if(tarjeta == "visa"):
+#     recargo_visa = monto * 1.07
+#     print("monto total:", recargo_visa)
+# elif(tarjeta == "mastercard"):
+#     recargo_master = monto * 1.11
+#     print("monto total:", recargo_master)
+# else:
+#     print("monto total", monto)
+
+#Ejercicio 9
+
+
+#Ejercicio 10
+# numero = int(input("ingrese un numero"))
+
+# if(numero % 2 == 0 or numero % 5 == 0):
+#     print(numero ** 3)
+# else:
+#     print("no es multiplo de 2 ni de 5")
+
+#Ejercicio 11

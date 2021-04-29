18a29664c6f7a03be153493e4a665545e82127a3
JoaquínC <81444960+JoaquinCabral@users.noreply.github.com>
2021-04-29 11:19:21 -0300

    Add files via upload

---
+++ clase3.py
@@ -0,0 +1,68 @@
+#Ejercicio 1
+# for i in range(0, 10):
+#     numero = int(input("ingrese un numero"))
+#     if(numero % 2 == 0 and numero % 3 == 0):
+#         print("es multiplo de 2 y de 3")
+
+#Ejercicio 2
+# for i in range(0, 15):
+#     nombre = input("ingrese su nombre ").capitalize()
+#     año_nacimiento = int(input("ingrese año de nacimiento"))
+
+#     edad = 2021 - año_nacimiento
+
+#     if(edad >= 18):
+#         print("es mayor de edad")
+#     else:
+#         print("es menor de edad")
+
+#Ejercicio 3
+# for i in range(0, 10):
+#     nombre = input("ingrese su nombre ").capitalize()
+
+
+#Ejercicio 4
+# suma = 0
+
+# aprobados = 0
+
+# for i in range(0, 3):
+#     nota = float(input("ingrese la nota del alumno"))
+    
+
+#     if(nota >= 6):
+#         suma = suma + nota
+#         aprobados = aprobados + 1
+#         print("aprobó la materia")
+#     else:
+#         print("no aprobó la materia")
+
+# print("el promedio es", suma/aprobados)
+
+#Ejercicio 6
+# maximo = int(input("ingrese un numero"))
+
+# for i in range(0, 4):
+#     numero = int(input("ingrese un numero "))
+
+#     if(numero > maximo):
+#         maximo = numero
+
+# print("el maximo es", maximo)
+
+#Ejercicio 7
+# positivos = 0
+# negativos = 0
+
+# numero = int(input("ingrese un numero "))
+
+# while(numero != 0):
+#     if(numero > 0):
+#         positivos = positivos + 1
+#     else:
+#         negativos = negativos + negativos
+
+#     numero = int(input("ingrese un numero"))
+
+# print("cantidad de positivos", positivos)
+# print("cantidad de negativos", negativos)
